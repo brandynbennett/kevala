59526e7ba907b6b24a8b0c2d359682f8b44ee38b
brandynbennett <brandynlbennett@gmail.com>
2022-03-12 11:45:15 -0700

    clean up csv_test with helper function

--- test/csv_test.exs
+++ test/csv_test.exs
@@ -2,40 +2,41 @@ defmodule Kevala.CSVTest do
   use ExUnit.Case, async: true
 
   test "decode/2 decodes a csv" do
-    csv = ~s("foo","bar"\n"baz","qux")
-    {:ok, stream} = StringIO.open(csv)
+    csv = stream_csv(~s("foo","bar"\n"baz","qux"))
 
-    assert stream |> IO.binstream(:line) |> Kevala.CSV.decode() |> Enum.to_list() == [
+    assert Kevala.CSV.decode(csv) |> Enum.to_list() == [
              {:ok, ["foo", "bar"]},
              {:ok, ["baz", "qux"]}
            ]
   end
 
   test "decode/2 accepts options" do
-    csv = ~s("foo","bar"\n"baz","qux")
-    {:ok, stream} = StringIO.open(csv)
+    csv = stream_csv(~s("foo","bar"\n"baz","qux"))
 
-    assert stream |> IO.binstream(:line) |> Kevala.CSV.decode(headers: true) |> Enum.to_list() ==
+    assert Kevala.CSV.decode(csv, headers: true) |> Enum.to_list() ==
              [
                {:ok, %{"foo" => "baz", "bar" => "qux"}}
              ]
   end
 
   test "decode/2 shows errors" do
-    csv = ~s("\"foo","bar"\n"baz","qux")
-    {:ok, stream} = StringIO.open(csv)
+    csv = stream_csv(~s("\"foo","bar"\n"baz","qux"))
 
-    assert stream |> IO.binstream(:line) |> Kevala.CSV.decode() |> Enum.to_list() == [
+    assert Kevala.CSV.decode(csv) |> Enum.to_list() == [
              {:error, "Stray quote on line 1 near \"\""},
              {:ok, ["baz", "qux"]}
            ]
   end
 
   test "decode/2 puts duplicate columns into list" do
-    csv = ~s("foo","foo"\n"baz","qux")
-    {:ok, stream} = StringIO.open(csv)
+    csv = stream_csv(~s("foo","foo"\n"baz","qux"))
 
-    assert stream |> IO.binstream(:line) |> Kevala.CSV.decode(headers: true) |> Enum.to_list() ==
+    assert Kevala.CSV.decode(csv, headers: true) |> Enum.to_list() ==
              [ok: %{"foo" => ["baz", "qux"]}]
+  end
+
+  defp stream_csv(csv) do
+    {:ok, stream} = StringIO.open(csv)
+    stream |> IO.binstream(:line)
   end
 end
