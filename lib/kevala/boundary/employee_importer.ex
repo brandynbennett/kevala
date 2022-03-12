9bfa8d320f16a17e459fcc1014801e83cc6242ed
brandynbennett <brandynlbennett@gmail.com>
2022-03-12 12:23:43 -0700

    validate missing headers

--- lib/kevala/boundary/employee_importer.ex
+++ lib/kevala/boundary/employee_importer.ex
@@ -1,5 +1,63 @@
 defmodule Kevala.Boundary.EmployeeImporter do
-  def remove_duplicates(csv, duplicate_detection_strategy \\ :email_or_phone) do
-    Kevala.CSV.decode(csv, headers: true) |> Enum.to_list()
+  @moduledoc """
+  Import employee data from a CSV
+  """
+
+  @expected_headers ["First Name", "Last Name", "Phone", "Email"]
+
+  def remove_duplicates(csv, _duplicate_detection_strategy \\ :email_or_phone) do
+    with {:ok, data} <- decode(csv),
+         :ok <- validate_headers(data) do
+      data
+    end
+  end
+
+  defp decode(csv) do
+    try do
+      {:ok, Kevala.CSV.decode(csv, headers: true) |> Enum.to_list()}
+    rescue
+      _error -> {:error, "CSV not parseable"}
+    end
+  end
+
+  defp validate_headers(data) do
+    with {:ok, row} <- first_valid_row(data),
+         {:ok, headers} <- get_headers(row),
+         :ok <- find_missing_headers(headers) do
+      :ok
+    end
+  end
+
+  defp first_valid_row(data) do
+    row = Enum.find(data, &(elem(&1, 0) == :ok))
+    if row, do: {:ok, elem(row, 1)}, else: {:error, "No valid rows"}
+  end
+
+  defp get_headers(row) do
+    {:ok, Map.keys(row)}
+  end
+
+  defp find_missing_headers(headers) do
+    difference = find_header_difference(headers)
+    expected = MapSet.new(@expected_headers)
+    headers = MapSet.new(headers)
+
+    if MapSet.subset?(expected, headers) do
+      :ok
+    else
+      {:error, "Headers #{difference} are required, but were not provided"}
+    end
+  end
+
+  defp find_header_difference(headers) do
+    expected = MapSet.new(@expected_headers)
+    headers = MapSet.new(headers)
+
+    MapSet.difference(expected, headers)
+    |> Enum.to_list()
+    |> Enum.join(",")
+  end
+
+  defp to_csv(data) do
   end
 end
--- test/boundary/employee_importer_test.exs
+++ test/boundary/employee_importer_test.exs
@@ -4,21 +4,25 @@ defmodule Kevala.Boundary.EmployeeImporterTest do
   alias Kevala.Boundary.EmployeeImporter
   alias Kevala.Core.Employee
 
+  test "remove_duplicates/2 returns error if not parseable" do
+    assert EmployeeImporter.remove_duplicates(:foo) == {:error, "CSV not parseable"}
+  end
+
+  test "remove_duplicates/2 returns error if missing columns" do
+    csv = stream_csv(~s("First Name","Email"\n"Marge","marge@simpsons.com"))
+
+    assert EmployeeImporter.remove_duplicates(csv) ==
+             {:error, "Headers Last Name,Phone are required, but were not provided"}
+  end
+
   test "remove_duplicates/2 ignores problem-free CSV" do
     csv =
       stream_csv(
         ~s("First Name","Last Name","Email","Phone"\n"Marge","Simpson","marge@simpsons.com","999-999-9999")
       )
 
-    assert EmployeeImporter.remove_duplicates(csv) == [
-             {:ok,
-              %{
-                "First Name" => "Marge",
-                "Last Name" => "Simpson",
-                "Email" => "marge@simpsons.com",
-                "Phone" => "999-999-9999"
-              }}
-           ]
+    assert EmployeeImporter.remove_duplicates(csv) ==
+             ~s("First Name","Last Name","Email","Phone"\n"Marge","Simpson","marge@simpsons.com","999-999-9999")
   end
 
   defp stream_csv(csv) do
